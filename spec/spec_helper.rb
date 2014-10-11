98bcd9c1ab6ffc84a054e0c4c3cce6c47a1cdddd
Zen Kyprianou <zen@kyprianou.eu>
2014-10-11 01:33:32 +0100

    Add production_code helper method to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,8 @@ SimpleCov.start do
   add_filter '/vendor/'
   coverage_dir 'log/coverage/spec'
 end
+
+def production_code
+  spec = caller[0][/spec.+\.rb/]
+  './' + spec.gsub('_spec','').gsub(/spec/, 'lib')
+end
