2c8250d24208ddd6d3854d9fc8e5982af62e7941
Zen Kyprianou <zen@kyprianou.eu>
2014-10-11 03:43:22 +0100

    ensure the wrappers are inside Snmpjr

--- lib/snmpjr/wrappers/smi.rb
+++ lib/snmpjr/wrappers/smi.rb
@@ -1,7 +1,9 @@
 require 'snmpjr/wrappers/snmp4j'
 
-module Wrappers
-  module SMI
-    include_package 'org.snmp4j.smi'
+class Snmpjr
+  module Wrappers
+    module SMI
+      include_package 'org.snmp4j.smi'
+    end
   end
 end
