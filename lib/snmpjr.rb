5c242fd865f4d1b1e5f9fa9a758cf9b1d17ef64c
Zen Kyprianou <zen@kyprianou.eu>
2014-10-11 02:13:35 +0100

    Implement get top level method

--- lib/snmpjr.rb
+++ lib/snmpjr.rb
@@ -1,5 +1,20 @@
 require "snmpjr/version"
+require "snmpjr/pdu"
+require "snmpjr/session"
+require "snmpjr/target"
 
-module Snmpjr
-  # Your code goes here...
+class Snmpjr
+
+  def initialize options = {}
+    @host = options.fetch(:host)
+    @port = options.fetch(:port) || 161
+    @community = options.fetch(:community)
+  end
+
+  def get oid
+    target = Snmpjr::Target.new(:host => @host, :port => @port, :community => @community)
+    pdu = Snmpjr::Pdu.new oid
+    Snmpjr::Session.new.send target, pdu
+  end
+
 end
