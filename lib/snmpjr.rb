c029ae7936c6444733a36e335254e926e4e5cc1f
Zen Kyprianou <zen@kyprianou.eu>
2014-10-12 14:32:08 +0100

    Map over an array of Oids and get them synchronously

--- lib/snmpjr.rb
+++ lib/snmpjr.rb
@@ -11,8 +11,23 @@ class Snmpjr
     @community = options.fetch(:community)
   end
 
-  def get oid
+  def get oids
     target = Snmpjr::Target.new.create(:host => @host, :port => @port, :community => @community)
+    case oids.class.to_s
+    when 'String'
+      get_oid(oids, target)
+    when 'Array'
+      oids.map{|oid|
+        get_oid(oid, target)
+      }
+    else
+      raise ArgumentError.new 'You can request a single Oid using a String, or multiple using an Array'
+    end
+  end
+
+  private
+
+  def get_oid oid, target
     pdu = Snmpjr::Pdu.new.create oid
     Snmpjr::Session.new.send(pdu, target)
   end
