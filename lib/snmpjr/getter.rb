6114e695ddf1ad2ff24f06a4a6322449a218980b
Zen Kyprianou <zen@kyprianou.eu>
2014-10-14 18:44:22 +0100

    Update getter to perform one get for a bunch of oids

--- lib/snmpjr/getter.rb
+++ lib/snmpjr/getter.rb
@@ -4,31 +4,32 @@ require "snmpjr/session"
 class Snmpjr
   class Getter
 
-    def initialize target
-      @target = target
+    def initialize args = {}
+      @target = args.fetch(:target)
+      @max_oids_per_request = args[:max_oids_per_request] || 30
       @session = Snmpjr::Session.new
     end
 
     def get_multiple oids
       @session.start
-      result = oids.map {|oid|
-        send_oid oid
-      }
+      result = oids.each_slice(@max_oids_per_request).map{|partial_oids|
+        get_request partial_oids
+      }.flatten
       @session.close
       result
     end
 
     def get oid
       @session.start
-      result = send_oid oid
+      result = get_request [oid]
       @session.close
-      result
+      result.first
     end
 
     private
 
-    def send_oid oid
-      pdu = Snmpjr::Pdu.new.create oid
+    def get_request oids
+      pdu = Snmpjr::Pdu.new.create oids
       @session.send(pdu, @target)
     end
 
--- spec/snmpjr/getter_spec.rb
+++ spec/snmpjr/getter_spec.rb
@@ -4,25 +4,28 @@ describe Snmpjr::Getter do
   let(:target) { double :target }
   let(:session) { double Snmpjr::Session }
   let(:pdu) { double Snmpjr::Pdu }
-  let(:created_pdu_1) { double :created_pdu_1 }
+  let(:created_pdu_single) { double :created_pdu_single }
 
-  subject { described_class.new(target) }
+  subject { described_class.new(:target => target, :max_oids_per_request => 1) }
 
   before do
     allow(Snmpjr::Pdu).to receive(:new).and_return pdu
-    allow(pdu).to receive(:create).with('1.2.3.4.5.6').and_return created_pdu_1
+    allow(pdu).to receive(:create).with(['1.2.3.4.5.6']).and_return created_pdu_single
     allow(Snmpjr::Session).to receive(:new).and_return session
     allow(session).to receive(:start)
-    allow(session).to receive(:send).with(created_pdu_1, target).and_return 'Foo'
+    allow(session).to receive(:send).with(created_pdu_single, target).and_return ['Foo']
     allow(session).to receive(:close)
   end
 
   describe "#get_multiple" do
-    let(:created_pdu_2) { double :created_pdu_2 }
+    let(:created_pdu_multiple_1) { double :created_pdu_multiple_1 }
+    let(:created_pdu_multiple_2) { double :created_pdu_multiple_2 }
 
     before do
-      allow(pdu).to receive(:create).with('6.5.4.3.2.1').and_return created_pdu_2
-      allow(session).to receive(:send).with(created_pdu_2, target).and_return 'Bar'
+      allow(pdu).to receive(:create).with(['1.2.3.4.5.6']).and_return created_pdu_multiple_1
+      allow(pdu).to receive(:create).with(['6.5.4.3.2.1']).and_return created_pdu_multiple_2
+      allow(session).to receive(:send).with(created_pdu_multiple_1, target).and_return ['Foo']
+      allow(session).to receive(:send).with(created_pdu_multiple_2, target).and_return ['Bar']
     end
 
     it 'starts an snmp session' do
@@ -32,8 +35,8 @@ describe Snmpjr::Getter do
 
     it 'performs multiple gets for each oid' do
       expect(subject.get_multiple ['1.2.3.4.5.6', '6.5.4.3.2.1']).to eq(['Foo', 'Bar'])
-      expect(session).to have_received(:send).with(created_pdu_1, target)
-      expect(session).to have_received(:send).with(created_pdu_2, target)
+      expect(session).to have_received(:send).with(created_pdu_multiple_1, target)
+      expect(session).to have_received(:send).with(created_pdu_multiple_2, target)
     end
 
     it 'closes the snmp session' do
@@ -50,7 +53,7 @@ describe Snmpjr::Getter do
 
     it 'performs a synchronous get' do
       expect(subject.get '1.2.3.4.5.6').to eq 'Foo'
-      expect(session).to have_received(:send).with(created_pdu_1, target)
+      expect(session).to have_received(:send).with(created_pdu_single, target)
     end
 
     it 'closes the snmp session' do
