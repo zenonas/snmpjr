2c7b6931651b696d4c09289ba324a0a9332c5adc
Zen Kyprianou <zen@kyprianou.eu>
2014-10-14 18:50:33 +0100

    Modify pdu to accept an array of oids

--- lib/snmpjr/pdu.rb
+++ lib/snmpjr/pdu.rb
@@ -1,14 +1,19 @@
+require 'snmpjr/wrappers/smi'
+require 'snmpjr/wrappers/snmp4j'
+
 class Snmpjr
   class Pdu
     module Constants
       GET = -96
     end
 
-    def create oid
+    def create oids
       pdu = Snmpjr::Wrappers::PDU.new
-      oid = Snmpjr::Wrappers::SMI::OID.new oid
-      variable_binding = Snmpjr::Wrappers::SMI::VariableBinding.new oid
-      pdu.add variable_binding
+      oids.map {|oid|
+        oid = Snmpjr::Wrappers::SMI::OID.new oid
+        variable_binding = Snmpjr::Wrappers::SMI::VariableBinding.new oid
+        pdu.add variable_binding
+      }
       pdu.type = Snmpjr::Pdu::Constants::GET
       pdu
     end
