6445a3afa8274465928b48576a06cb516a97085a
Zen Kyprianou <zen@kyprianou.eu>
2014-10-16 17:28:46 +0100

    Remove begin rescue around requiring java
    gemspec will not allow the gem to be installed on anything other than jruby

--- lib/snmpjr/wrappers/snmp4j.rb
+++ lib/snmpjr/wrappers/snmp4j.rb
@@ -1,11 +1,7 @@
 class Snmpjr
   module Wrappers
-    begin
-      require 'java'
-      require 'snmpjr/wrappers/snmp4j-2.3.1.jar'
-    rescue
-      raise "Snmpjr requires JRuby"
-    end
+    require 'java'
+    require 'snmpjr/wrappers/snmp4j-2.3.1.jar'
 
     include_package 'org.snmp4j'
   end
