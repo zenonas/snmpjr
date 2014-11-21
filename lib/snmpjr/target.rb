6a6f252f40196d64e1d168aab1f4b5d223bd2735
Zen Kyprianou <zen@kyprianou.eu>
2014-11-21 17:03:13 +0000

    Make target create a user target as well as a community target

--- lib/snmpjr/target.rb
+++ lib/snmpjr/target.rb
@@ -1,10 +1,18 @@
 require 'snmpjr/wrappers/smi'
 require 'snmpjr/target_v2c'
+require 'snmpjr/target_v3'
+require 'snmpjr/configuration_v2c'
+require 'snmpjr/configuration_v3'
 
 class Snmpjr
   class Target
+    POSSIBLE_TARGETS = {
+      Snmpjr::ConfigurationV2C => Snmpjr::TargetV2C,
+      Snmpjr::ConfigurationV3 => Snmpjr::TargetV3
+    }
+
     def create(configuration)
-      Snmpjr::TargetV2C.new.create(configuration)
+      POSSIBLE_TARGETS.fetch(configuration.class).new.create(configuration)
     end
   end
 end
--- spec/snmpjr/target_spec.rb
+++ spec/snmpjr/target_spec.rb
@@ -1,23 +1,60 @@
 require production_code
-require 'snmpjr/configuration_v2c'
 
 describe Snmpjr::Target do
 
   describe '#create' do
-    let(:configuration) do
-      config = Snmpjr::ConfigurationV2C.new
-      config.host = '127.0.0.1'
-      config.port = 161
-      config.community = 'some community'
-      config.retries = 2
-      config.timeout = 50
-      config
+    context 'when we have a V2C config' do
+      let(:configuration) do
+        config = Snmpjr::ConfigurationV2C.new
+        config.host = '127.0.0.1'
+        config.port = 161
+        config.community = 'some community'
+        config.retries = 2
+        config.timeout = 50
+        config
+      end
+
+      let(:target_v2c) { double :target_v2c }
+      let(:community_target) { double :community_target }
+
+      before do
+        allow(Snmpjr::TargetV2C).to receive(:new).and_return(target_v2c)
+        allow(target_v2c).to receive(:create).with(configuration).and_return community_target
+      end
+
+      it 'returns a V2C Target' do
+        expect(Snmpjr::Target.new.create(configuration)).to eq(community_target)
+      end
     end
 
-    context 'when given a version 2 config' do
-      it 'returns a V2C Target' do
-        expect(Snmpjr::Target.new.create(configuration)).to be_a(Snmpjr::Wrappers::CommunityTarget)
+    context 'when we have a V3 config' do
+      let(:configuration) do
+        config = Snmpjr::ConfigurationV3.new
+        config.host = '127.0.0.1'
+        config.port = 161
+        config.context = '80004fb805636c6f75644dab22cc'
+        config.user = 'username'
+        config.security_level = 'authPriv'
+        config.authentication_protocol = 'SHA'
+        config.authentication_key = 'authkey1'
+        config.privacy_protocol = 'DES'
+        config.privacy_key = 'privkey1'
+        config.timeout = 50
+        config
       end
+
+      let(:target_v3) { double :target_v3 }
+      let(:user_target) { double :user_target }
+
+      before do
+        allow(Snmpjr::TargetV3).to receive(:new).and_return(target_v3)
+        allow(target_v3).to receive(:create).with(configuration).and_return user_target
+      end
+
+      it 'returns a V3 target' do
+        expect(Snmpjr::Target.new.create(configuration)).to eq(user_target)
+      end
+
     end
   end
 end
