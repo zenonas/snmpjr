85acd820cb6ab745befa4d684f9612f15b69cdd6
Dan Lesser <daniel.lesser@bt.com>
2015-01-14 15:28:54 +0000

    Support ASN.1 types for SNMP walk

--- spec/integration/snmp_v3/snmpjr_walk_spec.rb
+++ spec/integration/snmp_v3/snmpjr_walk_spec.rb
@@ -20,8 +20,9 @@ describe "snmpjr for snmp v3" do
       it 'can perform a simple synchronous walk request on an snmp agent' do
         response = subject.walk '1.3.6.1.2.1.1'
         expect(response.count).to eq 11
-        expect(response.first.to_s).to eq 'SunOS zeus.snmplabs.com 4.1.3_U1 1 sun4m'
-        expect(response.last.to_s).to match(/^\d+\:\d+:\d+\.\d+$/)
+        expect(response.first.to_h).to eq(
+          { oid: '1.3.6.1.2.1.1.1.0', value: 'SunOS zeus.snmplabs.com 4.1.3_U1 1 sun4m', type: 'OCTET STRING' }
+        )
       end
 
       context "when a non existent subtree is walked" do
